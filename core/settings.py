3c53428c0ce131768977aa6a537b15a821dbb8c1
Upm-Technologies-git <upmtechnologies@gmail.com>
2025-07-05 14:09:07 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -118,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # not the same as STATICFILES_DIRS
+
+# Optional, if you have additional static directories
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
